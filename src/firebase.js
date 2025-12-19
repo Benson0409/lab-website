83b4abe23719d5c16c69f8d3d09fb63902205aa7
Benson0409 <benson5508@gmail.com>
2025-12-19 22:33:54 +0800

    調整 api 的隱私權限
    讓firebase的資料不會被上傳到網路上

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 // 【請注意】這裡需要換成你從 Firebase Console 拿到的真實設定
 // 如果暫時沒有，保持這樣也可以讓網頁先跑起來（但無法登入後台）
 const firebaseConfig = {
-  apiKey: "AIzaSyDBnfhFc0mbXE0cFpEteTb2hfDeezLksQA",
-  authDomain: "lab-website-5f952.firebaseapp.com",
-  projectId: "lab-website-5f952",
-  storageBucket: "lab-website-5f952.firebasestorage.app",
-  messagingSenderId: "456843068100",
-  appId: "1:456843068100:web:21ade159f0166ba0897f91",
-  measurementId: "G-BPL4QQNRWP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
